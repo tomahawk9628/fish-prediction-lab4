8124a0c0a4654a76bba65c117effe14d54a6e0ad
Dennis Issac <dissac80@gmail.com>
2022-07-28 21:31:17 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+#Import main library
+import numpy as np
+
+#Import Flask modules
+from flask import Flask, request, render_template
+
+#Import pickle to save our regression model
+import pickle 
+
+#Initialize Flask and set the template folder to "template"
+app = Flask(__name__, template_folder = 'template')
+
+#Open our model 
+model = pickle.load(open('model.pkl','rb'))
+
+#create our "home" route using the "index.html" page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#Set a post method to yield predictions on page
+@app.route('/', methods = ['POST'])
+def predict():
+    
+    #obtain all form values and place them in an array, convert into integers
+    int_features = [float(x) for x in request.form.values()]
+    #Combine them all into a final numpy array
+    final_features = [np.array(int_features)]
+    #predict the price given the values inputted by user
+    prediction = model.predict(final_features)
+    
+    #Round the output to 2 decimal places
+    output = prediction[0]
+    
+    #If the output is negative, the values entered are unreasonable to the context of the application
+    #If the output is greater than 0, return prediction
+
+    return render_template('index.html', prediction_text = "The fish belongs to " + (output) + " species.")   
+
+#Run app
+if __name__ == "__main__":
+    app.run(debug=True)
